627e5d6020dc8222c6e2df310a9b8e56251b8f4c
BuildTools <puffinlump@gmail.com>
2021-03-07 19:17:33 -0500

    Remove Anticheat folder

--- Anticheat/src/Starter.server.lua
+++
@@ -1,7 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local LocalLinker = ReplicatedStorage:FindFirstChild("LocalLinker")
-
--- Run outside of Hexolus environment
-if not LocalLinker then
-	require(script.Parent)()
-end
